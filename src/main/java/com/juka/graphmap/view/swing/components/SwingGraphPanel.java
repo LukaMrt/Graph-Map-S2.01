6c23db2a24f4c9d9142779fbd52d301ec8e0fb9a
Julien Linget <julien.linget@proton.me>
2022-05-30 22:51:44 +0200

    feat: add draw graph panel

--- src/main/java/com/juka/graphmap/view/swing/components/SwingGraphPanel.java
+++ src/main/java/com/juka/graphmap/view/swing/components/SwingGraphPanel.java
@@ -1,10 +1,13 @@
 package com.juka.graphmap.view.swing.components;
 
+import com.juka.graphmap.domain.model.link.Link;
+import com.juka.graphmap.domain.model.node.Coordinate;
 import com.juka.graphmap.domain.model.node.Node;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 import java.util.List;
 
 public class SwingGraphPanel extends JPanel {
@@ -21,7 +24,44 @@ public class SwingGraphPanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        Ellipse2D.Double ellipse = new Ellipse2D.Double(10,10,50,50);
-        g2d.draw(ellipse);
+        drawLinks(g2d, nodes);
+        drawNodes(g2d, nodes);
+    }
+
+    private void drawLinks(Graphics2D g2d, List<Node> nodes) {
+        Coordinate nodeCoordinate, neighborCoordinate;
+        for (Node node : nodes) {
+            nodeCoordinate = node.getCoordinate();
+
+            for (Link neighborLink : node.getNeighborsLinks()) {
+                neighborCoordinate = neighborLink.getDestination().getCoordinate();
+
+                switch (neighborLink.getType()) {
+                    case HIGHWAY -> g2d.setColor(new Color(255,0,0));
+                    case NATIONAL -> g2d.setColor(new Color(0,255,0));
+                    case DEPARTMENTAL -> g2d.setColor(new Color(0,0,255));
+                }
+                g2d.draw(new Line2D.Double(nodeCoordinate.x() + 10, nodeCoordinate.y() + 10,
+                        neighborCoordinate.x()+ 10, neighborCoordinate.y() + 10));
+            }
+
+        }
+    }
+
+    private void drawNodes(Graphics2D g2d, List<Node> nodes) {
+        Coordinate nodeCoordinate;
+        for (Node node : nodes) {
+            nodeCoordinate = node.getCoordinate();
+
+            g2d.setColor(new Color(0,0,0));
+            g2d.drawString(node.getName(), (nodeCoordinate.x() - node.getName().length() * 3.3f), (nodeCoordinate.y() - 5));
+
+            switch (node.getType()) {
+                case CITY -> g2d.setColor(new Color(0,255,0));
+                case RECREATION_CENTER -> g2d.setColor(new Color(0,0,255));
+                case RESTAURANT -> g2d.setColor(new Color(255,0,0));
+            }
+            g2d.fill(new Ellipse2D.Double(node.getCoordinate().x(), node.getCoordinate().y(), 20, 20));
+        }
     }
 }
